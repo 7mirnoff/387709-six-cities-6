0c0d0ab8947728d870e45d818043949dfef76dcb
Smirnov Dmitrey <dima7mirnoff@gmail.com>
2021-02-09 17:12:47 +0300

    Добавляет стейт активной карточки и обработчик наведения на карточку

--- src/components/places-list/places-list.jsx
+++ src/components/places-list/places-list.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import PlaceCard from '../place-card/place-card';
 
 const PlacesList = ({hotels}) => {
+  const [idActive, setIdActive] = useState(hotels[0].id);
+
+  const handleMouseEnter = (id) => {
+    setIdActive(id);
+  };
+
   return (
     <>
       <div className="cities__places-list places__list tabs__content">
         {hotels.map((hotel) =>
           <PlaceCard
             key={hotel.id}
+            id={hotel.id}
             title={hotel.title}
             image={hotel.preview_image}
             price={hotel.price}
@@ -16,6 +23,7 @@ const PlacesList = ({hotels}) => {
             type={hotel.type}
             isFavorite={hotel.is_favorite}
             isPremium={hotel.is_premium}
+            handleMouseEnter={handleMouseEnter}
           />)}
       </div>
     </>
