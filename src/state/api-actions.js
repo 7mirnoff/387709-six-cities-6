8d28380c48cae728e3555e7a87374ef13c760855
Smirnov Dmitrey <dima7mirnoff@gmail.com>
2021-03-10 18:47:09 +0300

    Добавляет logout при загрузке страницы /login

--- src/state/api-actions.js
+++ src/state/api-actions.js
@@ -1,4 +1,4 @@
-import {AuthorizationActionTypes} from "./authorization/action";
+import {AuthorizationActionCreator} from "./authorization/action";
 import {CitiesActionCreator} from "./cities/action";
 import {AuthorizationStatus} from "../utils";
 
@@ -9,12 +9,18 @@ export const fetchHotelsList = () => (dispatch, _getState, api) => (
 
 export const checkAuth = () => (dispatch, _getState, api) => (
   api.get(`/login`)
-    .then(() => dispatch(AuthorizationActionTypes.requireAuthorization(AuthorizationStatus.AUTH)))
+    .then(() => dispatch(AuthorizationActionCreator.requireAuthorization(AuthorizationStatus.AUTH)))
     .catch(() => {})
 );
 
 export const login = ({login: email, password}) => (dispatch, _getState, api) => (
   api.post(`/login`, {email, password})
-    .then(() => dispatch(AuthorizationActionTypes.requireAuthorization(AuthorizationStatus.AUTH)))
-    .then(() => dispatch(AuthorizationActionTypes.redirectToRoute(`/favorites`)))
+    .then(() => dispatch(AuthorizationActionCreator.requireAuthorization(AuthorizationStatus.AUTH)))
+    .then(() => dispatch(AuthorizationActionCreator.redirectToRoute(`/favorites`)))
+);
+
+export const logout = () => (dispatch, _getState, api) => (
+  api.get(`/logout`)
+    .then(() => dispatch(AuthorizationActionCreator.requireAuthorization(AuthorizationStatus.NO_AUTH)))
+    .then(() => dispatch(AuthorizationActionCreator.redirectToRoute(`/login`)))
 );
